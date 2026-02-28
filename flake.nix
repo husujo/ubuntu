620f6acdcbbf9b49ec0bc16823fa64e516bcd4da
Hunter <husujo@users.noreply.github.com>
2026-02-28 19:48:33 +0700

    Add initial flake.nix configuration for CLI tools

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  #nixConfig = {
+  #  extra-substituters = ["https://<name>.cachix.org"];
+  #  extra-trusted-public-keys = ["<name>.cachix.org-1:<pub-key>"];
+  #};
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+
+      cliTools = with pkgs; [
+        #doctl
+        #google-cloud-sdk
+        flyctl
+        #s3cmd
+        bun
+	      #deno
+        nodejs_24
+        gh
+        ngrok
+        #expect
+        rustup
+        #<other>.packages.${system}.<name>
+        opencode
+        rclone
+        rustup
+        # ollama-cuda (doesn't work)
+      ];
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "cli-tools";
+        paths = cliTools;
+      };
+    };
+}
+
+## how to update nix package manager
+# sudo su
+# nix-env --install --file '<nixpkgs>' --attr nix cacert -I nixpkgs=channel:nixpkgs-unstable
+# systemctl daemon-reload
+# systemctl restart nix-daemon
