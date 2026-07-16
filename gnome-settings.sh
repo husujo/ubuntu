68c38958cfa405c04dd6c5fd41890e3d80e30d06
Hunter <husujo@users.noreply.github.com>
2026-07-16 18:47:27 -0400

    Update gnome-settings.sh to configure extensions

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -38,3 +38,14 @@ echo "(sudo) remove imagemagick from search results:"
 echo "NoDisplay=true" | sudo tee -a /usr/share/applications/display-im6.q16.desktop
 
 command -v system76-power &> /dev/null && system76-power profile performance
+
+# extension list found with `gnome-extensions list --enabled`
+# removed: rounded-window-corners@fxgn user-theme@gnome-shell-extensions.gcampax.github.com auto-move-windows@gnome-shell-extensions.gcampax.github.com
+EXT_LIST=(just-perfection-desktop@just-perfection extension-list@tu.berry blur-my-shell@aunetx Vitals@CoreCoding.com clipboard-indicator@tudmotu.com scroll-workspaces@gfxmonk.net); for i in "${EXT_LIST[@]}"; do busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${i}; done
+
+# https://www.gnome-look.org/p/1316887
+# download theme, unzip, move to .theme/, open tweaks
+
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com/schemas/ set org.gnome.shell.extensions.clipboard-indicator toggle-menu "['<Super>c']"
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com/schemas/ set org.gnome.shell.extensions.vitals hot-sensors "['_memory_usage_', '_processor_usage_', '_network_public_ip_']"
+gnome-extensions disable Vitals@CoreCoding.com && gnome-extensions enable Vitals@CoreCoding.com
